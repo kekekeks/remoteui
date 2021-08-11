67a9e00f964ae03c927025f4cf778da3d07135ca
artyom <worldbeater-dev@yandex.ru>
2021-08-11 14:44:46 +0300

    Fix strict TypeScript compilation errors

--- RemoteUi/web/src/RemoteUiEditor.tsx
+++ RemoteUi/web/src/RemoteUiEditor.tsx
@@ -421,7 +421,7 @@ interface CustomSelect {
 class RemoteUiEditorContext
 {
     @observable.ref remoteUiEditorCustomSelect?: CustomSelect;
-    @observable highlightErrors: boolean;
+    @observable highlightErrors: boolean = false;
     @observable.ref customization?: IRemoteUiEditorCustomization;
 }
 
