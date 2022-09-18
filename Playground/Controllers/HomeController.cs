4b776e9a41ff8551ea76bb827dae7e2c6522e4b5
Artyom <worldbeater-dev@yandex.ru>
2022-09-18 16:16:04 +0300

    chore: Resurrect Playground

--- Playground/Controllers/HomeController.cs
+++ Playground/Controllers/HomeController.cs
@@ -1,84 +1,122 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using RemoteUi;
 
-namespace Playground.Controllers
+namespace Playground.Controllers;
+
+[Route("")]
+public class HomeController : Controller
 {
-    [Route("")]
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+    private readonly RemoteUiBuilder<Dto> _info;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private RemoteUiBuilder<Dto> _info;
-        private RemoteUiBuilder _oldInfo;
-        public HomeController()
-        {
-            // Old example
-            _oldInfo = new RemoteUiBuilder(typeof(Dto), null, null, new SnakeCaseNamingStrategy())
-                .Register(typeof(DtoBase), null, "Test");
+        _logger = logger;
+
+        // Old example
+        var oldInfo = new RemoteUiBuilder(typeof(Dto), null, null, new SnakeCaseNamingStrategy())
+            .Register(typeof(DtoBase), null, "Test");
             
-            _info = new RemoteUiBuilder<Dto>(null, null, new SnakeCaseNamingStrategy())
-                .Register<DtoBase>(null, "Test");
-        }
-        [HttpGet("")]
-        public IActionResult Index()
+        _info = new RemoteUiBuilder<Dto>(null, null, new SnakeCaseNamingStrategy())
+            .Register<DtoBase>(null, "Test")
+            .Register<ReadOnlyDtoBase>()
+            .Register<ReadOnlyInnerDto>();
+    }
+
+    [HttpGet("")]
+    public IActionResult Index() => Redirect("/index.html");
+
+    [HttpGet("description")]
+    public object GetDescription() => _info.Build(null);
+
+    [HttpGet("initial")]
+    public object GetObject() =>
+        new JsonResult(new Dto
         {
-            return Redirect("/index.html");
-        }
-
-        [HttpGet("description")]
-        public object GetDescription() => _info.Build(null);
-
-        [HttpGet("initial")]
-        public object GetObject() => new JsonResult(new Dto() { SomeString = "Sample Text" }, _info.GetSerializerSettings());
-
-        [HttpPost("validate")]
-        public object Validate([FromBody] Dto data)
-        {
-
-            return new JsonResult(new
+            SomeString = "Sample Text",
+            ReadOnlyExample = new ReadOnlyDtoBase
             {
-                SomeString = "Error in string",
-                ListOfStrings = new[]
+                ListOfReadOnlyStrings = new List<ReadOnlyInnerDto>
                 {
-                    null, "Error in second element"
-                },
-                ListOfObjects = new object[]
-                {
-                    new {SomeString = "Error in first object"},
-                    new {ListStrings = new[] {"Error in second object"}}
-                }
-            }, _info.GetSerializerSettings());
-        }
-    }
+                    new()
+                    {
+                        Title = "One"
+                    },
+                    new()
+                    {
+                        Title = "Two"
+                    },
+                } 
+            }
+        }, _info.GetSerializerSettings());
 
-    public class Dto : DtoBase
+    [HttpPost("validate")]
+    public object Validate([FromBody] Dto data)
     {
-        
+        _logger.LogInformation(JObject.FromObject(data).ToString(Formatting.Indented));
+        return new JsonResult(new
+        {
+            SomeString = "Error in string",
+            ListOfStrings = new[]
+            {
+                null, "Error in second element"
+            },
+            ListOfObjects = new object[]
+            {
+                new
+                {
+                    SomeString = "Error in first object"
+                },
+                new
+                {
+                    ListStrings = new[] {"Error in second object"}
+                }
+            }
+        }, _info.GetSerializerSettings());
     }
-    
-    [RemoteUiFieldGroup("", "Common")]
-    public class DtoBase
-    {
-        [RemoteUiField("List of strings")]
-        public List<string> ListOfStrings { get; set; }
+}
+
+public class Dto : DtoBase { }
+
+[RemoteUiFieldGroup("", "Common")]
+public class DtoBase
+{
+    [RemoteUiField("List of strings")]
+    public List<string> ListOfStrings { get; set; }
         
-        [RemoteUiField("Overided ID List", Id = "OverrideMe")]
-        [JsonProperty("OverrideMe")]
-        public List<string> OverrideList { get; set; } = new List<string>() { "123321" };
+    [RemoteUiField("Overided ID List", Id = "OverrideMe")]
+    [JsonProperty("OverrideMe")]
+    public List<string> OverrideList { get; set; } = new() { "123321" };
         
-        [RemoteUiField("SomeString")]
-        public string SomeString { get; set; }
+    [RemoteUiField("SomeString")]
+    public string SomeString { get; set; }
         
-        [RemoteUiField("ListOfObjects")]
-        public List<DtoBase> ListOfObjects { get; set; }
+    [RemoteUiField("ListOfObjects")]
+    public List<DtoBase> ListOfObjects { get; set; }
         
-        [RemoteUiField("Some float")]
-        public decimal SomeFloat { get; set; }
-    }
+    [RemoteUiField("Some float")]
+    public decimal SomeFloat { get; set; }
+
+    [RemoteUiField("Read Only Model", ReadOnly = true)]
+    public ReadOnlyDtoBase ReadOnlyExample { get; set; }
+}
+
+public class ReadOnlyDtoBase
+{
+    [RemoteUiField("Read only string", ReadOnly = true)]
+    public string ReadOnlyString { get; set; }
+
+    [RemoteUiField("List of read only strings", ReadOnly = true, ListType = typeof(ReadOnlyInnerDto))]
+    public List<ReadOnlyInnerDto> ListOfReadOnlyStrings { get; set; }
+}
+
+public class ReadOnlyInnerDto
+{
+    [RemoteUiField("Read only title", ReadOnly = true)]
+    public string Title { get; set; }
 }
--- Playground/Startup.cs
+++ Playground/Startup.cs
@@ -1,48 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 
-namespace Playground
+namespace Playground;
+
+public class Startup
 {
-    public class Startup
+    public void ConfigureServices(IServiceCollection services) => services.AddControllers().AddNewtonsoftJson();
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        public Startup(IConfiguration configuration)
+        app.UseStaticFiles(new StaticFileOptions
         {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-            
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            app.UseStaticFiles(new StaticFileOptions()
-            {
-                FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "webapp/dist"))
-            });
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+            FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "webapp/dist"))
+        });
+        app.UseRouting();
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
     }
 }
